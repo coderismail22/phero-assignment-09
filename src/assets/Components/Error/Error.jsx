f8459201f01c530bafcd8c3df1f205156b3efc8a
Coder Ismail <coderismail22@gmail.com>
2023-04-12 22:27:32 +0600

    error page

---
+++ src/assets/Components/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div className='w-full h-screen bg-slate-500'>
+            <h1 className='my-0 mx-auto text-red-700 text-8xl'>Error 404</h1>
+            <h1 className='my-0 mx-auto text-red-700 text-8xl'>Page Not Found</h1>
+        </div>
+    );
+};
+
+export default Error;
